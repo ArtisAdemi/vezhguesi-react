c2e5863a179ac2a0e405d3dd03029db3bbbd3fee
Florian Berisha <florian.fani@hotmail.com>
2024-10-13 12:23:38 +0200

    resolved conflicts

--- vezhguesi/src/pages/VerifyEmail.tsx
+++ vezhguesi/src/pages/VerifyEmail.tsx
@@ -1,36 +1,36 @@
-import React, { useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import AuthService from '../services/Auth';
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import AuthService from "../services/Auth";
 
 const VerifyEmail: React.FC = () => {
-    const navigate = useNavigate();
-    const { token } = useParams();
+  const navigate = useNavigate();
+  const { token } = useParams();
 
-    useEffect(() => {
-        const verifyEmail = async () => {
-            console.log("token", token);
-            if (token) {
-                try {
-                    await AuthService.verifyEmail({ token });
-                    alert('Email verified successfully!');
-                    navigate('/dashboard'); // Redirect to dashboard
-                } catch (error) {
-                    console.error('Verification failed:', error);
-                    alert('Email verification failed. Please try again.');
-                }
-            } else {
-                alert('Invalid verification link.');
-            }
-        };
+  useEffect(() => {
+    const verifyEmail = async () => {
+      console.log("token", token);
+      if (token) {
+        try {
+          await AuthService.verifyEmail({ token });
+          alert("Email verified successfully!");
+          navigate("/"); // Redirect to dashboard
+        } catch (error) {
+          console.error("Verification failed:", error);
+          alert("Email verification failed. Please try again.");
+        }
+      } else {
+        alert("Invalid verification link.");
+      }
+    };
 
-        verifyEmail();
-    }, [navigate, token]);
+    verifyEmail();
+  }, [navigate, token]);
 
-    return (
-        <div className="verify-email">
-            <h1>Verifying your email...</h1>
-        </div>
-    );
+  return (
+    <div className="verify-email">
+      <h1>Verifying your email...</h1>
+    </div>
+  );
 };
 
 export default VerifyEmail;
