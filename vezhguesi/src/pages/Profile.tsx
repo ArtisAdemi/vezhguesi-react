83886a9c19bace4648a0811eabf5f3ee8e298370
Genc VH <florian.fani@hotmail.com>
2024-10-15 13:51:11 +0200

    profile UI

--- vezhguesi/src/pages/Profile.tsx
+++ vezhguesi/src/pages/Profile.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import profile from "../assets/profile.jpg"
 const Profile: React.FC = () => {
     return (
         <div className='w-[98%]'>
@@ -10,12 +10,12 @@ const Profile: React.FC = () => {
                     <h1>Payments</h1>
                 </div>
             </div>
-            <div className='flex py-10 bg-white mt-8 '>
-                <div className='leftSide w-[40%] px-[6%] flex flex-col justify-center'>
+            <div className='flex bg-white mt-8 '>
+                <div className='leftSide w-[45%] px-[6%] border-2 border-l-0 border-t-0 border-b-0 flex flex-col py-12 justify-center'>
                     <div className='flex flex-col items-center mb-4'>
 
                         <h1 className='text-[#6C6C6C] text-3xl mb-4'>Profile</h1>
-                        <img src="" alt="" />
+                        <img src={profile} alt="" />
                         <h1 className='font-bold text-3xl'>Florian Berisha</h1>
                         <h1 className='text-[#6C6C6C] text-xl'>lorem ipsum 2</h1>
                     </div>
@@ -34,7 +34,35 @@ const Profile: React.FC = () => {
                         </div>
                     </div>
                 </div>
-                <div className='rightSide'></div>
+                <div className='rightSide w-[55%] px-[6%] py-24'>
+                    <form action="" className='pt-6'>
+                        <div className='flex justify-between border-2 border-l-0 border-r-0 border-t-0 pb-6'>
+                            <h1 className='font-bold text-lg'>Account Information</h1>
+                            <div className='flex gap-x-4'>
+                                <button className='bg-slate-400 rounded-lg px-5 py-1 text-white'>Edit</button>
+                                <button className='bg-[#0C7FDA] rounded-lg px-5 py-1 text-white font-semibold text-sm'>SAVE</button>
+                            </div>
+                        </div>
+                        <div className='flex gap-x-4 mt-6 justify-between'>
+                            <div className='flex flex-col w-[55%]'>
+                                <label htmlFor="" className='mb-3'>FIRST NAME</label>
+                                <input type="text" name="" id="" className='border-2 border-[#E5E5E5] rounded-md py-1' />
+                            </div>
+                            <div className='flex flex-col w-[45%]'>
+                                <label htmlFor="" className='mb-3'>LAST NAME</label>
+                                <input type="text" name="" id="" className='border-2 border-[#E5E5E5] rounded-md py-1' />
+                            </div>
+                        </div>
+                        <div className='flex flex-col mt-3'>
+                            <label htmlFor="" className='mb-3'>lorem</label>
+                            <input type="text" name="" id="" className='border-2 border-[#E5E5E5] rounded-md py-1' />
+                        </div>
+                        <div className='flex flex-col mt-3'>
+                            <label htmlFor="" className='mb-3'>EMAIL</label>
+                            <input type="text" name="" id="" className='border-2 border-[#E5E5E5] rounded-md py-1' />
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     )
