6a4e0d70568993c340ae63e6d2ddfc60bbbefa59
ArtisAdemi <artis.ademi@gmail.com>
2024-10-17 18:38:46 +0200

    Fixed backend api url configuration

--- vezhguesi/src/services/Auth.ts
+++ vezhguesi/src/services/Auth.ts
@@ -7,13 +7,13 @@ class AuthService {
 
     async signup(signupRequest: SignupRequest): Promise<void> {
         console.log("API_URL", API_URL);
-        const response = await axios.post(`${API_URL}/users/`, signupRequest);
+        const response = await axios.post(`${API_URL}/auth/`, signupRequest);
         return response.data;
     }
 
     async verifyEmail(req: VerifyEmailRequest): Promise<void> {
         console.log("token", req.token);
-        const response = await axios.get(`${API_URL}/users/verify-signup/${req.token}`);
+        const response = await axios.get(`${API_URL}/auth/verify-signup/${req.token}`);
         return response.data;
     }
 }
--- vezhguesi/src/services/backendUrl.ts
+++ vezhguesi/src/services/backendUrl.ts
@@ -1,1 +1,11 @@
-export const API_URL = process.env.NODE_ENV === 'stage' ? process.env.API_URL_STAGE : process.env.API_URL_TEST;
+let API_URL: string;
+
+if (import.meta.env.VITE_NODE_ENV === 'stage' && import.meta.env.VITE_API_URL_STAGE) {
+    API_URL = import.meta.env.VITE_API_URL_STAGE;
+} else if (import.meta.env.VITE_NODE_ENV === 'test' && import.meta.env.VITE_API_URL_TEST) {
+    API_URL = import.meta.env.VITE_API_URL_TEST;
+} else {
+    API_URL = "http://localhost:3001/api";
+}
+
+export { API_URL }; 
