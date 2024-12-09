3966109b1e04948ded10752da6d777764c25468f
Florian <florian.fani@hotmail.com>
2024-12-09 15:34:14 +0100

    Charts, Trending and small changes

--- vezhguesi/src/pages/Dashboard.tsx
+++ vezhguesi/src/pages/Dashboard.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import LineChart from '../components/LineChart'
-import data from '../assets/data/mockData'
-import mockSentimentData from '../assets/data/mockSentimentData'
+// import LineChart from '../components/LineChart'
+// import data from '../assets/data/mockData'
+//import mockSentimentData from '../assets/data/mockSentimentData'
 import { useAuth } from '../hooks/useAuth'
 import BotActivity from '../components/BotActivity'
+import Trending from '../components/Trending'
+import Charts from '../components/Charts'
+
 
 
 const Dashboard: React.FC = () => {
@@ -16,27 +19,26 @@ const Dashboard: React.FC = () => {
             <h1 className='hidden md:block'>Your Token: {authToken}</h1>
             {/* TESTING AREA */}
             <div className='flex flex-col md:flex-row justify-between '>
-                <div className='h-[400px] shadow-md rounded-lg bg-white w-[100%] md:h-[400px] md:w-[50%] md:px-3'>
-                    <LineChart
-                        data={data}
-                        xAxisLabel="Month"
-                        yAxisLabel="Mentions"
-                    />
+                <div className='h-[400px]  rounded-lg w-[100%] md:h-[400px] md:w-[50%] md:px-3'>
+                    <Charts />
                 </div>
-                <div className='w-full mt-5 md:mt-0 md:w-[25%]'>
+                <div className='w-full mt-40 md:mt-0 md:w-[25%]'>
                     <BotActivity />
                 </div>
             </div>
-            <div className='h-[400px] mt-5 shadow-md rounded-lg bg-white w-[100%] md:h-[400px] md:w-[50%] md:px-3'>
+            <div>
+                <Trending />
+            </div>
+            {/* <div className='h-[400px] mt-5 shadow-md rounded-lg bg-white w-[100%] md:h-[400px] md:w-[50%] md:px-3'>
                 <LineChart
                     data={mockSentimentData}
                     xAxisLabel="Month"
                     yAxisLabel="Sentiment"
                 />
-            </div>
-            <div className='h-[400px] w-[400px] mx-auto md:mx-0'>
-                {/* <PieChart data={transformedData} /> */}
-            </div>
+            </div> */}
+            {/* <div className='h-[400px] w-[400px] mx-auto md:mx-0'>
+                <PieChart data={transformedData} />
+            </div> */}
         </div>
     )
 }
