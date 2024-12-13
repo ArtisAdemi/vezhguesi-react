1757b95d91d3e8ab59e019ae2bca430b34072247
ArtisAdemi <artis.ademi@gmail.com>
2024-12-13 17:40:29 +0100

    org service

--- vezhguesi/src/services/Auth.ts
+++ vezhguesi/src/services/Auth.ts
@@ -1,31 +1,31 @@
 import axios from "axios";
 import { LoginRequest, LoginResponse, SignupRequest, UpdateUserDataRequest, UserData, VerifyEmailRequest } from "../models/Auth";
-import { API_URL } from "./backendUrl";
+import { MAIN_API_URL } from "./backendUrl";
 
 class AuthService {
 
     async signup(signupRequest: SignupRequest): Promise<void> {
-        const response = await axios.post(`${API_URL}/auth/`, signupRequest);
+        const response = await axios.post(`${MAIN_API_URL}/auth/`, signupRequest);
         return response.data;
     }
 
     async verifyEmail(req: VerifyEmailRequest): Promise<void> {
-        const response = await axios.get(`${API_URL}/auth/verify-signup/${req.token}`);
+        const response = await axios.get(`${MAIN_API_URL}/auth/verify-signup/${req.token}`);
         return response.data;
     }
 
     async login(loginRequest: LoginRequest): Promise<LoginResponse> {
-        const response = await axios.post(`${API_URL}/auth/login`, loginRequest);
+        const response = await axios.post(`${MAIN_API_URL}/auth/login`, loginRequest);
         return response.data;
     }
 
     async getUserData(token: string): Promise<UserData> {
-        const response = await axios.get(`${API_URL}/users/user-data`, { headers: { Authorization: `Bearer ${token}` } });
+        const response = await axios.get(`${MAIN_API_URL}/users/user-data`, { headers: { Authorization: `Bearer ${token}` } });
         return response.data;
     }
 
     async updateUserData(req: UpdateUserDataRequest, token: string, setUser: (user: UserData) => void): Promise<void> {
-        const response = await axios.put(`${API_URL}/auth/update`, req, { headers: { Authorization: `Bearer ${token}` } });
+        const response = await axios.put(`${MAIN_API_URL}/auth/update`, req, { headers: { Authorization: `Bearer ${token}` } });
         const newUserData: LoginResponse = {
             token: token,
             userData: response.data
@@ -35,14 +35,14 @@ class AuthService {
     }
 
     async forgotPassword(email: string): Promise<void> {
-        const response = await axios.post(`${API_URL}/auth/forgot-password`, { email });
+        const response = await axios.post(`${MAIN_API_URL}/auth/forgot-password`, { email });
         return response.data;
     }
 
     async resetPassword(token: string, password: string, confirmPassword: string): Promise<void> {
         const newPassword = password;
         const confirmNewPassword = confirmPassword;
-        const response = await axios.put(`${API_URL}/auth/reset-password/${token}`, { newPassword, confirmNewPassword });
+        const response = await axios.put(`${MAIN_API_URL}/auth/reset-password/${token}`, { newPassword, confirmNewPassword });
         return response.data;
     }
 }
